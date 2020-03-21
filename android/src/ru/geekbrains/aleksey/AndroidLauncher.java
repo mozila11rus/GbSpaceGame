3b60ce810df8371b8ac96e60bf19a4a355cfdea0
Aleksey Chizhov <8mozila8@rambler.ru>
2020-03-21 15:01:39 +0300

    First commit

---
+++ android/src/ru/geekbrains/aleksey/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.aleksey;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.geekbrains.aleksey.SpaceGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceGame(), config);
+	}
+}
